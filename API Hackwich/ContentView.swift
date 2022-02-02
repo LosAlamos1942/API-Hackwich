5b04d4b63c65fc8b4f2ecb9c16e443a680d6072a
Nerto Kageh <kagehnerto@yahoo.com>
2022-02-02 10:42:06 -0600

    Finished setting up alert.

--- API Hackwich/ContentView.swift
+++ API Hackwich/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [Joke]()
-    
+    @State private var showingAlert = false
     
     
     var body: some View {
@@ -26,6 +26,11 @@ struct ContentView: View {
         }
         .onAppear(perform: {
             getJokes()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
         })
     }
     func getJokes() {
@@ -44,9 +49,11 @@ struct ContentView: View {
                         let joke = Joke(setup: setup, punchline: punchline)
                         jokes.append(joke)
                     }
+                    return
                 }
             }
         }
+        showingAlert = true 
     }
     
     
